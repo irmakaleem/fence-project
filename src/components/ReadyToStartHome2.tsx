cf12286f6ef3352aaa4b32ce16bde00edc3b4f51
irmakaleem <irmakaleem12@gmail.com>
2026-02-21 09:40:01 +0500

    Added vercel rewrite for routing

--- src/components/ReadyToStartHome2.tsx
+++ src/components/ReadyToStartHome2.tsx
@@ -3,11 +3,11 @@ import react from "react";
 const ReadyToStartHome2: React.FC = () => {
   return (
     <div className="w-full bg-[#F8F1D9] py-24 px-4 md:px-10  ">
-      <div className="  xl:max-w-[72%] 2xl:max-w-[68%] mx-auto relative z-[2] ">
+      <div className="  xl:max-w-[78%] 2xl:max-w-[68%] mx-auto relative z-[2] ">
         <div className="relative  rounded-[40px] bg-gradient-to-r from-[#3A2C1D] to-[#7B551F] px-8  md:px-16  flex flex-col md:flex-row items-center justify-between xl:min-h-[553px]  2xl:min-h-[653px] ">
           {/* Left Content */}
           <div className="max-w-lg text-left text-white/80">
-            <h2 className="text-3xl md:text-4xl lg:text-[70px] font-semibold leading-tight">
+            <h2 className="text-3xl md:text-4xl xl:text-[60px] 2xl:text-[70px] font-semibold leading-tight">
               Ready to <br />
               start your <br />
               project?
@@ -25,7 +25,7 @@ const ReadyToStartHome2: React.FC = () => {
           </div>
 
           {/* Right Image */}
-          <div className="absolute top-[-2%] right-[-35%]  xl:h-[800px] xl:w-[782px] 2xl:h-[923px] 2xl:w-[982px] ">
+          <div className="absolute top-[-28%] right-[-12%]  xl:h-[800px] xl:w-[720px] 2xl:h-[923px] 2xl:w-[982px] ">
             <img
               src="/assets/grassman.png"
               alt="Ready to start"
